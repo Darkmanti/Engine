53ada8d002ba6257061c1cc2873c4f9b588b7503
unknown <unknownamem@gmail.com>
2019-01-15 12:41:24 +0700

    Добавил парочку компонентов в движок. Отредактировал .gitignore под свои нужды. Изменения не критичные. К сбою не приведут. Доабвил структуру будущего окна движка. Начало положено :)

--- Engine/Engine/main.cpp
+++ Engine/Engine/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <windows.h>
 
-int main()
+typedef  struct
 {
+	UINT style; 			// Стиль окна
+	WNDPROC lpfnWndProc 	// Указатель на оконную процедуру
+	int32_t cbClsExtra; 	// Дополнительные байты после класса
+	int32_t cbWndExtra; 	// Дополнительные байты после экземпляра окна
+	HINSTANCE hInstance; 	// Дескриптор окна. (Потом передаем его как параметр в WinMain)
+	HICON hIcon; 			// Дескриптор иконки приложения
+	HCURSOR hCursor; 		// Курсор приложения
+	HBRUSH hbrBackground; 	// Фоновый цвет
+	LPCTSTR lpszMenuName; 	// Наименование меню
+	LPCTSTR lpszClassName; 	// Наименование класса окна
+} WNDCLASS, *PWNDcLASS;
 
-	system("pause");
 
+
+int WinMain()
+{
+	
 	return 0;
 }
